076716677369e470704002360c4cfe2dd1729f8e
Abdulaleem Abdullah Ali Mohammed Al-patool <ab0odsoftwar@gmail.com>
2026-06-06 20:48:49 +0300

    Update RepositoryManager.cs

--- Infrastructure/ECommerce.Repository/Implementation/RepositoryManager.cs
+++ Infrastructure/ECommerce.Repository/Implementation/RepositoryManager.cs
@@ -4,66 +4,44 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Repository;
 
-
 public class RepositoryManager : IRepositoryManager
 {
+    private readonly RepositoryContext _repository;
 
+    private readonly Lazy<ICartRepository> _cart;
+    private readonly Lazy<IOrderRepository> _order;
+    private readonly Lazy<IProductRepository> _product;
+    private readonly Lazy<IPaymentRepository> _payment;
+    private readonly Lazy<ICategoryRepository> _category;
+    private readonly Lazy<IIdempotencyRecordRepository> _idempotency;
+    private readonly Lazy<IWebHookEventRepository> _webHookEventRepository;
+    private readonly Lazy<IPaymentTransactionRepository> _paymentTransaction;
+    private readonly Lazy<IUserPermissionRepository> _userPermission;  // إضافة Lazy جديدة
 
-   private readonly RepositoryContext _repository;
-   private readonly Lazy<ICartRepository> _cart;
-
-private readonly Lazy<IOrderRepository> _order;
-   private readonly Lazy<IProductRepository> _product;
-      private readonly Lazy<IPaymentRepository> _payment;
-         private readonly Lazy<ICategoryRepository> _category;
-         
-           private readonly Lazy<IIdempotencyRecordRepository> _idempotency;
-           private readonly Lazy<IWebHookEventRepository> _webHookEventRepository;
-           private readonly Lazy<IPaymentTransactionRepository> _paymentTransaction;
-private IUserPermissionRepository? _userPermissionRepository;
-
-
- public RepositoryManager(RepositoryContext repository)
+    public RepositoryManager(RepositoryContext repository)
     {
-        _repository=repository;
-         _cart=new Lazy<ICartRepository>(()=>new CartRepository(repository));
-         _order=new Lazy<IOrderRepository>(()=>new OrderRepository(repository));
-         _product=new Lazy<IProductRepository>(()=>new ProductRepository(repository));
-         _category=new Lazy<ICategoryRepository>(()=>new CategoryRepository(repository));
-      
-        _payment=new Lazy<IPaymentRepository>(()=>new PaymentRepository(repository));
-          _paymentTransaction = new Lazy<IPaymentTransactionRepository>(() => new PaymentTransactionRepository(repository));
+        _repository = repository;
+        _cart = new Lazy<ICartRepository>(() => new CartRepository(repository));
+        _order = new Lazy<IOrderRepository>(() => new OrderRepository(repository));
+        _product = new Lazy<IProductRepository>(() => new ProductRepository(repository));
+        _category = new Lazy<ICategoryRepository>(() => new CategoryRepository(repository));
+        _payment = new Lazy<IPaymentRepository>(() => new PaymentRepository(repository));
+        _paymentTransaction = new Lazy<IPaymentTransactionRepository>(() => new PaymentTransactionRepository(repository));
         _webHookEventRepository = new Lazy<IWebHookEventRepository>(() => new WebhookEventRepository(repository));
         _idempotency = new Lazy<IIdempotencyRecordRepository>(() => new IdempotencyRecordRepository(repository));
-        
-    _userPermissionRepository ??= new UserPermissionRepository(repository);
-
+        _userPermission = new Lazy<IUserPermissionRepository>(() => new UserPermissionRepository(repository)); // تهيئة Lazy
     }
 
-    public ICartRepository CartRepository =>_cart.Value;
-
-    public IPaymentRepository PaymentRepository =>_payment.Value;
-
-
-
-    public IProductRepository ProductRepository => _product.Value ;
-
-    public IOrderRepository OrderRepository => _order.Value ;
-
-    public ICategoryRepository CategoryRepository => _category.Value ;
-    
-  public IPaymentTransactionRepository PaymentTransactionRepository => _paymentTransaction.Value;
-  //  public IWebHookEventRepository WebhookEventRepository => _webHookEventRepository.Value;
+    public ICartRepository CartRepository => _cart.Value;
+    public IPaymentRepository PaymentRepository => _payment.Value;
+    public IProductRepository ProductRepository => _product.Value;
+    public IOrderRepository OrderRepository => _order.Value;
+    public ICategoryRepository CategoryRepository => _category.Value;
+    public IPaymentTransactionRepository PaymentTransactionRepository => _paymentTransaction.Value;
     public IIdempotencyRecordRepository IdempotencyRecordRepository => _idempotency.Value;
-
     public IWebHookEventRepository WebHookEventRepository => _webHookEventRepository.Value;
+    public IUserPermissionRepository UserPermissionRepository => _userPermission.Value; // إضافة الخاصية
 
-
-    public async Task SaveAsync()=>await _repository.SaveChangesAsync();
-
-public async Task<IDbContextTransaction> BeginTransactionAsync()=>await _repository.Database.BeginTransactionAsync();
-
-
+    public async Task SaveAsync() => await _repository.SaveChangesAsync();
+    public async Task<IDbContextTransaction> BeginTransactionAsync() => await _repository.Database.BeginTransactionAsync();
 }
-
-
